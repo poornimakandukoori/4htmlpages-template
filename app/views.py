068557ba673cc837c1fb83aa558ff41f194deeb4
poornimakandukoori <poornimakandukuri2610@gmail.com>
2023-11-10 18:21:13 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def first(request):
+    return render(request,'myhtml.html')
+def second(request):
+    return render(request,'myhtml.html')
+def third(request):
+    return render(request,'myhtml.html')
+def fourth(request):
+    return render(request,'myhtml.html')
